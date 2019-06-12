53f7a10d6542f82ecf833520e2b6b7a3742141ff
Uday Kurien <ukurien@gra-login2.graham.sharcnet>
2019-06-12 17:29:08 -0400

    Explicity specified literals for echo

--- menu_F.sh
+++ menu_F.sh
@@ -4,36 +4,36 @@
 
 scriptPath="/home/ukurien/projects/def-yaumanko/ukurien/ED50/Scripts/Final"
 
-echo Do you want to:
-echo 1: Prep for next run
-echo 2. Back up data
-echo 3. Launch simulation
-echo 4. Cancel
+echo 'Do you want to:\n'
+echo '1: Prep for next run'
+echo '2. Back up data'
+echo '3. Launch simulation'
+echo '4. Cancel'
 read varChoice
 
 if [ $varChoice -eq 1 ]
 then
 	# Launch preparation script
-	echo Transferring control to prep script
+	echo 'Transferring control to prep script'
 	sleep 2
 	$scriptPath/prep_for_next_run_F.sh
 
 elif [ $varChoice -eq 2 ]
 then
 	# Launch back up script
-	echo Transferring control to back up script
+	echo 'Script still to be written'
 	sleep 2
-	echo Work in progress
+	
 
 elif [ $varChoice -eq 3 ]
 then
 	# Launch simulation
-	echo Transferring control to simulation launch script
+	echo 'Transferring control to simulation launch script'
 	sleep 2
 	$scriptPath/launch_simulation_F.sh
 
 else
-	echo Cancelling
+	echo 'Cancelling'
 
 fi
 
