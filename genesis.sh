eca3a2e45c5110f7d01683c338bd2cfe390755cb
Uday Kurien <uday.kurien@gmail.com>
2019-06-12 18:46:42 -0400

    initial model clone script

---
+++ genesis.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# This script clones an initial copy of the model in the appropriate file hierarchy
+# for other scripts to operate on. Important flags and values (EDR, drop size etc.)
+# are modified to fit the hierarchy.
+
+
