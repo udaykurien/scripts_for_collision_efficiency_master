c41a47b21df1f27424d035ebc0370f40a6f94f33
Uday Kurien <uday.kurien@gmail.com>
2019-06-12 19:05:18 -0400

    genesis update

--- genesis.sh
+++ genesis.sh
@@ -4,4 +4,23 @@
 # for other scripts to operate on. Important flags and values (EDR, drop size etc.)
 # are modified to fit the hierarchy.
 
+echo "Enter path to parent model:"
+read parentPath
+echo "Enter lowerbound EDR:"
+read lbEDR
+echo "Enter upperbound EDR:"
+echo ubEDR
+echo "Enter EDR increments:"
+read incEDR
+echo "Enter lowerbound drop size:"
+read lbDropSize
+echo "Enter upperbound drop size:"
+read ubDropSize
+echo "Enter dropsize increments:"
+read incDropSize
+echo "Enter location of cloned models:"
+read baseClonePath
+
+
+
 
