3db2ac7c73a8a582fc4640d5f2af379fd550d983
Uday Kurien <uday.kurien@gmail.com>
2019-06-12 22:34:32 -0400

    genesis.sh updated

--- genesis.sh
+++ genesis.sh
@@ -6,21 +6,44 @@
 
 echo "Enter path to parent model:"
 read parentPath
+
 echo "Enter lowerbound EDR:"
 read lbEDR
 echo "Enter upperbound EDR:"
-echo ubEDR
+read ubEDR
 echo "Enter EDR increments:"
 read incEDR
+
 echo "Enter lowerbound drop size:"
 read lbDropSize
 echo "Enter upperbound drop size:"
 read ubDropSize
 echo "Enter dropsize increments:"
 read incDropSize
+
 echo "Enter location of cloned models:"
 read baseClonePath
 
+# Implementation of genesis:
+# Nested loops: Outer loop -> EDR, Inner loop -> Drop size
+# SED used to modify content of clones
 
+# Outer loop
+# ----------
+for (( EDR=$lbEDR; EDR<=$ubEDR; EDR=$EDR+$incEDR ))
+do
+	# Inner loop
+	# ----------
+	for (( DropSize=$lbDropSize; DropSize<=$ubDropSize; DropSize=$DropSize+$incDropSize ))
+	do
+		finalClonePath="$baseClonePath/$EDR/Rr$DropSize$DropSize/gomic0"
+		mkdir -pv $finalClonePath
+		cp -rv $parentPath/ $finalClonePath
 
+		# Checking and modifying cloned content to generate multiple model instances
+		# -------------------------------------------------------------------------
+		# Implementation:
+		# Use grep to check for the pattern
+		# If the pattern is present (grep exit status = 0) no changes are made
+		# If the pattern is absent (grep exit status != 0). SED is used to update the pattern
 
